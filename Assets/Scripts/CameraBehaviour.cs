c6f938bf86b5638d29439fc161a7149ecbb9a339
Alex Genzor <alexandros.genzor@fundacionmontessori.com>
2025-01-17 18:35:57 +0100

    Updated EnemyBehaviour Added code to EnemyBehaviour to allow enemy to go between the previously added waypoints

--- Assets/Scripts/CameraBehaviour.cs
+++ Assets/Scripts/CameraBehaviour.cs
@@ -1,11 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraBehaviour : MonoBehaviour
 {
     public Vector3 camOffset = new Vector3(0f, 1.2f, -2.6f);
     private Transform _target;
+    
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -1,30 +1,47 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.AI; // moviendo agentes enemigos 2
+
 public class EnemyBehaviour : MonoBehaviour
 {
-    // moviendo agentes enemigos \/\/\/\/
+    // moviendo agentes enemigos 1 \/\/\/\/
     public Transform patrolRoute;
     public List<Transform> locations;
     
-    // moviendo agentes enemigos /\/\/\/\
+    // moviendo agentes enemigos 1 /\/\/\/\
+
+    // moviendo agentes enemigos 2 \/\/\/\/
+    private int _locationIndex = 0;
+    private NavMeshAgent _agent;
+    
+    // moviendo agentes enemigos 2 /\/\/\/\
     
     // Start is called before the first frame update
     void Start()
     {
-        InitialitePatrolRoute(); // moviendo agentes enemigos
+        _agent = GetComponent<NavMeshAgent>(); // moviendo agentes enemigos 2
+        
+        InitialitePatrolRoute(); // moviendo agentes enemigos 1
+        
+        MoveToNextPatrolLocation(); // moviendo agentes enemigos 2
         
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        // moviendo agentes enemigos 3 \/\/\/\/
+        if (_agent.remainingDistance < 0.2f && !_agent.pathPending)
+        {
+            MoveToNextPatrolLocation();
+            
+        }
+        // moviendo agentes enemigos 3 /\/\/\/\
         
     }
     
-    // moviendo agentes enemigos \/\/\/\/
+    // moviendo agentes enemigos 1 \/\/\/\/
     void InitialitePatrolRoute()
     {
         foreach (Transform child in patrolRoute)
@@ -34,7 +51,22 @@ public class EnemyBehaviour : MonoBehaviour
         }
         
     }
-    // moviendo agentes enemigos /\/\/\/\
+    
+    // moviendo agentes enemigos 1 /\/\/\/\
+
+    // moviendo agentes enemigos 2 \/\/\/\/
+    void MoveToNextPatrolLocation()
+    {
+        if (locations.Count == 0) // moviendo agentes enemigos 3
+            return; // moviendo agentes enemigos 3
+        
+        _agent.destination = locations[_locationIndex].position;
+
+        _locationIndex = (_locationIndex + 1) % locations.Count; // moviendo agentes enemigos 3
+
+    }
+    
+    // moviendo agentes enemigos 2 /\/\/\/\
 
     private void OnTriggerEnter(Collider other)
     {
